640c534c0177fb3ff40e0b12369a7bb6b8e849a8
XiaoouPan <xip024@ucsd.edu>
2022-01-21 00:37:57 -0800

    multivariate case

--- match.R
+++ match.R
@@ -4,18 +4,12 @@ hist_match = function(y, x, n1, n, n.adapt = 5000, n.burn = 5000, n.iter = 10000
                              data = dat, 
                              inits = list(z = y,
                                           w = rep(1, n -  n1),
-                                          u01 = 0,
-                                          u02 = 0,
-                                          u11 = 0,
-                                          u12 = 0,
-                                          tau0 = 0.001,
-                                          tau1 = 0.001,
-                                          m0 = 0,
-                                          m1 = 0),
+                                          u0 = rep(0, 6),
+                                          u1 = rep(0, 6)),
                              n.adapt = n.adapt, quiet = TRUE), silent = TRUE)
   try(update(thismodel, n.burn, progress.bar = "none"), silent = TRUE)
   res.bugs = try(jags.samples(thismodel, 
-                              variable.names = c("w", "u01", "u02", "u11", "u12"),
+                              variable.names = c("w", "u0", "u1"),
                               n.iter = n.iter, progress.bar = "none"), silent = TRUE)
   w_rec = matrix(res.bugs$w, n, n.iter)
   return (list("w_rec" = w_rec))
