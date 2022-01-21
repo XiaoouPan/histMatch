2e1bae8dd130c8cc430e49f2dd491e58b07abdff
XiaoouPan <xip024@ucsd.edu>
2022-01-21 01:45:12 -0800

    code update

--- match.R
+++ match.R
@@ -1,16 +1,16 @@
 hist_match = function(y, x, n1, n, n.adapt = 5000, n.burn = 5000, n.iter = 10000) {
   dat = list(y = y, x = x, n1 = n1, n = n)
-  thismodel = try(jags.model(file = "bugs/match.txt", 
+  thismodel = jags.model(file = "bugs/match.txt", 
                              data = dat, 
                              inits = list(z = y,
                                           w = rep(1, n -  n1),
                                           u0 = rep(0, 6),
                                           u1 = rep(0, 6)),
-                             n.adapt = n.adapt, quiet = TRUE), silent = TRUE)
-  try(update(thismodel, n.burn, progress.bar = "none"), silent = TRUE)
-  res.bugs = try(jags.samples(thismodel, 
+                             n.adapt = n.adapt)
+  update(thismodel, n.burn)
+  res.bugs = jags.samples(thismodel, 
                               variable.names = c("w", "u0", "u1"),
-                              n.iter = n.iter, progress.bar = "none"), silent = TRUE)
+                              n.iter = n.iter)
   w_post = matrix(res.bugs$w, n - n1, n.iter)
   u0_post = matrix(res.bugs$u0, 6, n.iter)
   u1_post = matrix(res.bugs$u1, 6, n.iter)
--- test.R
+++ test.R
@@ -25,7 +25,7 @@ delta.con <- delta[index]
 #x.test.con = all.trial[index, c("SEX_ID", "ps", "WBC", "age_floor", "trant")]
 study = study[index]
 
-sapply(x.train.con, function(y) sum(is.na(y)))
+#sapply(x.train.con, function(y) sum(is.na(y)))
 miss.index = which(is.na(x.train.con$ps) | is.na(x.train.con$WBC))
 x.train.con = x.train.con[-miss.index, ]
 times.con = times.con[-miss.index]
@@ -34,28 +34,35 @@ delta.con = delta.con[-miss.index]
 study = study[-miss.index]
 
 ## A toy case with 20 samples
-toy.index = c(1:10, 255:264)
+toy.index = 1:400
 x.train.con$history = as.numeric(x.train.con$history) - 1
 x.train.con$SEX_ID = as.numeric(x.train.con$SEX_ID) - 1
 x.train.con$ps = as.numeric(x.train.con$ps) - 1
 x.train.con$trant = as.numeric(x.train.con$trant) - 1
+#x.train.con = data.matrix(x.train.con)
 x.train.con = data.matrix(x.train.con[toy.index, ])
 times.con = times.con[toy.index]
 delta.con = delta.con[toy.index]
 study = study[toy.index]
 
 ## Survival data processing
-surv.proc = surv.pre.bart(times = times.con, delta = delta.con, x.train = x.train.con, x.test = x.train.con, K = 3) 
+surv.proc = surv.pre.bart(times = times.con, delta = delta.con, x.train = x.train.con, x.test = x.train.con, K = 20) 
 y.train = surv.proc$y.train ## 40
 x.train = surv.proc$tx.train ## 40 * 7
+n = length(y.train)
+n1 = sum(x.train[, 2] == 0)
 x.train = x.train[, -2]
 
-fit = hist_match(y = y.train, x = x.train, n1 = 19, n = 40, n.adapt = 1000, n.burn = 1000, n.iter = 1000)
+fit = hist_match(y = y.train, x = x.train, n1 = n1, n = n, n.adapt = 1000, n.burn = 1000, n.iter = 1000)
 w_post = fit$w_post
 u0_post = fit$u0_post
 u1_post = fit$u1_post
 
-### rough check
-rowMeans(w_post)
-rowMeans(u0_post)
-rowMeans(u1_post)
+### survival probability at one year
+test.index = which(x.train[, 1] == 11.6725)
+w_est = c(rep(0, n1), rowMeans(w_post))
+beta0 = rowMeans(u0_post)
+beta1 = rowMeans(u1_post)
+mu_pred = (1 - w_est[test.index]) * (x.train[test.index, ] %*% beta0) + w_est[test.index] * (x.train[test.index, ] %*% beta1)
+prob_pred = pnorm(0, mu_pred, 1)
+mean(prob_pred)
