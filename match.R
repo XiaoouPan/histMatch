83a198647b0ccd130900dd562611d1e56bb108bd
XiaoouPan <xip024@ucsd.edu>
2022-01-26 00:00:48 -0800

    dummy variable for the ps

--- match.R
+++ match.R
@@ -4,15 +4,15 @@ hist_match = function(y, x, n1, n, n.adapt = 5000, n.burn = 5000, n.iter = 10000
                              data = dat, 
                              inits = list(z = y,
                                           w = rep(1, n -  n1),
-                                          u0 = rep(0, 6),
-                                          u1 = rep(0, 6)),
+                                          u0 = rep(0, 7),
+                                          u1 = rep(0, 7)),
                              n.adapt = n.adapt)
   update(thismodel, n.burn)
   res.bugs = jags.samples(thismodel, 
                               variable.names = c("w", "u0", "u1"),
                               n.iter = n.iter)
   w_post = matrix(res.bugs$w, n - n1, n.iter)
-  u0_post = matrix(res.bugs$u0, 6, n.iter)
-  u1_post = matrix(res.bugs$u1, 6, n.iter)
+  u0_post = matrix(res.bugs$u0, 7, n.iter)
+  u1_post = matrix(res.bugs$u1, 7, n.iter)
   return (list("w_post" = w_post, "u0_post" = u0_post, "u1_post" = u1_post))
 }
--- test.R
+++ test.R
@@ -18,11 +18,8 @@ study = all.trial$STUDY
 all.trial$history = as.factor(all.trial$STUDY != 1203)
 index = which(all.trial$arm == 0)
 x.train.con <- all.trial[index, c("history", "SEX_ID", "ps", "WBC", "age_floor", "trant")]
-#x.train.con <- all.trial[index, c("history", "age_floor")]
 times.con <- times[index]
 delta.con <- delta[index]
-## x.test.con for proximity score calculation
-#x.test.con = all.trial[index, c("SEX_ID", "ps", "WBC", "age_floor", "trant")]
 study = study[index]
 
 #sapply(x.train.con, function(y) sum(is.na(y)))
@@ -34,21 +31,17 @@ delta.con = delta.con[-miss.index]
 study = study[-miss.index]
 
 ## A toy case with 20 samples
-toy.index = 1:400
 x.train.con$history = as.numeric(x.train.con$history) - 1
 x.train.con$SEX_ID = as.numeric(x.train.con$SEX_ID) - 1
-x.train.con$ps = as.numeric(x.train.con$ps) - 1
+x.train.con$ps1 = as.numeric(x.train.con$ps == 1)
+x.train.con$ps2 = as.numeric(x.train.con$ps == 2)
 x.train.con$trant = as.numeric(x.train.con$trant) - 1
-#x.train.con = data.matrix(x.train.con)
-x.train.con = data.matrix(x.train.con[toy.index, ])
-times.con = times.con[toy.index]
-delta.con = delta.con[toy.index]
-study = study[toy.index]
+x.train.con = data.matrix(x.train.con[, -3])
 
 ## Survival data processing
 surv.proc = surv.pre.bart(times = times.con, delta = delta.con, x.train = x.train.con, x.test = x.train.con, K = 20) 
-y.train = surv.proc$y.train ## 40
-x.train = surv.proc$tx.train ## 40 * 7
+y.train = surv.proc$y.train ## 15295
+x.train = surv.proc$tx.train ## 15295 * 8
 n = length(y.train)
 n1 = sum(x.train[, 2] == 0)
 x.train = x.train[, -2]
@@ -58,11 +51,16 @@ w_post = fit$w_post
 u0_post = fit$u0_post
 u1_post = fit$u1_post
 
-### survival probability at one year
-test.index = which(x.train[, 1] == 11.6725)
+### survival probability
 w_est = c(rep(0, n1), rowMeans(w_post))
 beta0 = rowMeans(u0_post)
 beta1 = rowMeans(u1_post)
-mu_pred = (1 - w_est[test.index]) * (x.train[test.index, ] %*% beta0) + w_est[test.index] * (x.train[test.index, ] %*% beta1)
-prob_pred = pnorm(0, mu_pred, 1)
-mean(prob_pred)
+time_int = unique(x.train[, 1])
+prob_pred = rep(0, 20)
+for (i in 1:20) {
+  test.index = which(x.train[, 1] == time_int[i])
+  mu_pred = (1 - w_est[test.index]) * (x.train[test.index, ] %*% beta0) + w_est[test.index] * (x.train[test.index, ] %*% beta1)
+  prob_pred[i] = mean(pnorm(0, mu_pred, 1))
+}
+plot(time_int, prob_pred, type = "l")
+
